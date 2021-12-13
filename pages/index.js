120ff2989f3c77d313249a7c3ed4a18e83bf9eae
AlexMuia31 <alexmuia31@gmail.com>
2021-12-13 13:22:23 +0300

    banner beginning

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+const Banner = () => (
+  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+
+  </Flex>
+)
+
+
 export default function Home() {
   return (
     <div>
